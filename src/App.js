cb4a216b6c252151e107f1c35673582a45f9e048
Carson Beus <carson.beus@gmail.com>
2018-12-29 19:24:07 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
+
+class App extends Component {
+    
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyByD52BIwJ2AjumiGCS79yVTVd7ZUSGKIY",
+            authDomain: "auth-b1d04.firebaseapp.com",
+            databaseURL: "https://auth-b1d04.firebaseio.com",
+            projectId: "auth-b1d04",
+            storageBucket: "auth-b1d04.appspot.com",
+            messagingSenderId: "407114259975"
+        })
+    }
+    render() {
+        return (
+            <View>
+                <Header headerText="Authentication" />
+                <LoginForm />
+            </View>
+        )
+    }
+}
+
+export default App;
